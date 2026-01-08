f03bed510856453802877d694ab02d418d2774a5
Marnsil <marcos_zoo@hotmail.com>
2026-01-07 23:07:02 -0300

    solucionar algoritimos com IA

---
+++ codigos-com-copilot/palindromo.py
@@ -0,0 +1,7 @@
+palavra = input("Digite uma palavra: ")
+
+if palavra == palavra[::-1]:
+    print(f"'{palavra}' é um palíndromo")
+else:
+    print(f"'{palavra}' não é um palíndromo")
+    print(f"A palavra invertida é: '{palavra[::-1]}'")
---
+++ codigos-com-copilot/repet_txt.py
@@ -0,0 +1,6 @@
+string = input('Digite uma palavra: ')
+numero = int(input('Digite um número: '))
+
+mensagem = (string + '\n') * numero
+
+print(mensagem)
